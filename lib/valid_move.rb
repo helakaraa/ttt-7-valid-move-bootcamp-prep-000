f5ce06c9fbe71a8b17da35cf3cd51c64558536ef
hela karaa <hela.karaa@ensi-uma.tn>
2018-07-24 09:22:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
- 
- return position_taken?(board,index)
+ if index<0 || index>8 
+   return nil 
+ else
+   return position_taken?(board,index)
 end
 
 
